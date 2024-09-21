78520ab2178e7d145002dcbe8ae407b3e1bcf6cc
jusvicki <adeyokunanuoluwapo@yahoo.com>
2024-09-21 07:59:11 -0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+     <Navbar />
+    </>
+  )
+}
+
+export default App
